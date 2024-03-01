4e2d2d06a7541bf990b3cc38bcd302f7b2ec9d33
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-02-29 20:01:07 -0500

    Update client.js

--- client.js
+++ client.js
@@ -28939,6 +28939,7 @@ _0x2af9ee = aniChoice_startT + 1000 * timeoutS;
   anO.rad=anO.oRad=anO.nRad= 25
 //screenIns_objsEdible.push(anO)
  )
+			screenIns_objsEdible.push(anO)
  };  
  for (t = 0; t < edibAniTypes.length; t++){
  0   < edibAniTypes[t] && 0 == old_edibAniTypes[t] && (
@@ -28950,6 +28951,7 @@ _0x2af9ee = aniChoice_startT + 1000 * timeoutS;
   anO.rad=anO.oRad=anO.nRad= 25
   //screenIns_objsEdible.push(anO)
  )
+	 screenIns_objsEdible.push(anO)
  };
             
         screenIns_drawNewPlayerIns = aniT == a_mouse || aniT == a_shrimp; //draw basic instutions (eat green outlined, avoid red)
