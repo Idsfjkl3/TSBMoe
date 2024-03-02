083c944ae07e7a632f3d6f14f2063eb40e4fd83e
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-03-02 10:48:32 -0500

    Update client.js

--- client.js
+++ client.js
@@ -11648,7 +11648,12 @@ GameObj.prototype.drawCatfishBar = function() {
   if (this.specType2 > 0) {
 	    var eyeS = Math.max(1.0, this.rad / 25.0);
 	     var barH = 10 + 5 * eyeS;
-    this.drawBar("yellow", 1, this.specType2 * 100/3, 10);
+	    if (this.hpBarA > 0.001) {
+	      this.drawBar("yellow", 1, this.specType2 * 100/3, 10);
+	    } else {
+    this.drawBar("yellow", 1, this.specType2 * 100/3, barH);
+  }
+  }
 };
 
 GameObj.prototype.drawInfectionBar = function() {
