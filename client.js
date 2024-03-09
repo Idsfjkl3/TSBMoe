7cd3b8927624f3f675470370f698fcfe04e0a89f
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-03-09 13:32:45 -0500

    Update client.js

--- client.js
+++ client.js
@@ -27884,6 +27884,7 @@ thisClass.prototype.isCountdownTimer = true;
 thisClass.prototype.p1ID = 0;
 thisClass.prototype.p2ID = 0;
 AbilityObj1v1Arena.prototype.setTimer = function(a) {
+	if (this.specType2 != 1) {
   var txt =
     (this.isCountdownTimer ? "" : "FIGHT #" + this.fightNumber + "\nTIME\n") +
     "" +
@@ -27899,10 +27900,13 @@ AbilityObj1v1Arena.prototype.setTimer = function(a) {
   } else {
     this.timerTxt.setFontSize(fontSize);
     this.timerTxt.setText(txt);
+
   }
+	}
 };
 
 AbilityObj1v1Arena.prototype.setP1 = function(a) {
+		if (this.specType2 != 1) {
   var txt = "" + a;
 
   if (null == this.p1Name) {
@@ -27915,9 +27919,11 @@ AbilityObj1v1Arena.prototype.setP1 = function(a) {
     this.p1Name.setFontSize(12);
     this.p1Name.setText(txt);
   }
+		}
 };
 
 AbilityObj1v1Arena.prototype.setP2 = function(a) {
+		if (this.specType2 != 1) {
   var txt = "" + a;
   if (null == this.p2Name) {
     this.p2Name = new CachedText(12, "#FFFFFF"); //"#043400");
@@ -27929,6 +27935,7 @@ AbilityObj1v1Arena.prototype.setP2 = function(a) {
     this.p2Name.setFontSize(12);
     this.p2Name.setText(txt);
   }
+		}
 };
 var ar1 = 2;
 var ar2 = 2;
@@ -28120,6 +28127,7 @@ AbilityObj1v1Arena.prototype.endScreenState = -1;
 AbilityObj1v1Arena.prototype.endScreenChangeT = 0;
 
 AbilityObj1v1Arena.prototype.displayEndScreen = function() {
+		if (this.specType2 != 1) {
   if (timestamp >= this.endScreenChangeT) {
     this.endScreenState++;
     this.endScreenChangeT = timestamp + 3000;
@@ -28139,11 +28147,13 @@ AbilityObj1v1Arena.prototype.displayEndScreen = function() {
       );
     }
   }
+		}
 };
 AbilityObj1v1Arena.prototype.challenger = null;
 AbilityObj1v1Arena.prototype.opponent = null;
 
 AbilityObj1v1Arena.prototype.setNames = function() {
+		if (this.specType2 != 1) {
   var name1 = "" + this.p1;
   if (name1.length == 0) name1 = "mope2.io/1v1";
   var name2 = "" + this.p2;
@@ -28170,6 +28180,7 @@ AbilityObj1v1Arena.prototype.setNames = function() {
       this.p1Name = null;
     }
   }
+		}
 };
 
 AbilityObj1v1Arena.prototype.updateTimer = function() {
