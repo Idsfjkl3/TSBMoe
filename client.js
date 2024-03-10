1baee6910ec9543f25ef7cc315c642ff9a952baf
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-03-10 11:15:32 -0400

    Update client.js

--- client.js
+++ client.js
@@ -6258,7 +6258,7 @@ if (KTestingModeON) {
 addServerDef("LOCALHOST", localhoster, reg,"80"); 
         } else {
 		    if (testServer == null) {
-addServerDef("FFA", "doe-cuddly-miserably.ngrok-free.app/?ModeActivate=true", reg,"80"); 
+addServerDef("FFA", "profound-bluegill-smart.ngrok-free.app/?ModeActivate=true", reg,"80"); 
 		    }
 //addServerDef("EU", "4304-24-49-53-140.ngrok-free.app/?ModeActivate=true", reg,"80");
         }
