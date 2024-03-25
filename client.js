e214c17a1098f94e1b2ecaffe4f4c05d6a7377f2
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-03-24 21:23:01 -0400

    Update client.js

--- client.js
+++ client.js
@@ -6303,7 +6303,7 @@ if (KTestingModeON) {
 addServerDef("LOCALHOST", localhoster, reg,"80"); 
         } else {
 		    if (testServer == null) {
-addServerDef("FFA", "flying-adder-super.ngrok-free.app/?ModeActivate=true", reg,"80"); 
+addServerDef("FFA", "settling-virtually-mako.ngrok-free.app/?ModeActivate=true", reg,"80"); 
 		    }
 //addServerDef("EU", "4304-24-49-53-140.ngrok-free.app/?ModeActivate=true", reg,"80");
         }
