7b7f4a8fe833f6f069fe2cbdb6db9273d2b96d99
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-03-25 20:17:59 -0400

    Update client.js

--- client.js
+++ client.js
@@ -8099,7 +8099,7 @@ console.log(rps)
 
   ctx.beginPath();
   ctx.arc(0, 0, this.rad, 0, 2 * Math.PI, false);
-  ctx.arc(0, 0, this.rad * 0.9, 0, 2 * Math.PI, true);
+  ctx.arc(0, 0, this.specType, 0, 2 * Math.PI, true);
   ctx.fillStyle = "#FF4242";
   ctx.closePath();
   ctx.fill();
@@ -8111,8 +8111,8 @@ console.log(rps)
   ctx.save();
   ctx.globalAlpha = 0.3;
   ctx.beginPath();
-  ctx.arc(0, 0, this.rad * 0.9, 0, 2 * Math.PI, false);
-  ctx.arc(0, 0, this.rad * 0.9 - 2, 0, 2 * Math.PI, true);
+  ctx.arc(0, 0, this.specType, 0, 2 * Math.PI, false);
+  ctx.arc(0, 0, this.specType - 2, 0, 2 * Math.PI, true);
   ctx.fillStyle = "#FF0000";
   ctx.closePath();
   ctx.fill();
@@ -8121,8 +8121,8 @@ console.log(rps)
   ctx.save();
   ctx.globalAlpha = 0.3;
   ctx.beginPath();
-  ctx.arc(0, 0, this.rad * 0.7 - 100, 0, 2 * Math.PI, false);
-  ctx.arc(0, 0, this.rad * 0.7, 0, 2 * Math.PI, true);
+  ctx.arc(0, 0, this.specType2 - 100, 0, 2 * Math.PI, false);
+  ctx.arc(0, 0, this.specType2, 0, 2 * Math.PI, true);
   ctx.fillStyle = "#32ff00";
   ctx.closePath();
   ctx.fill();
