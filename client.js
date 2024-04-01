1a228dbb90d7e7b59f724cc554631c5ef6fcc701
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-04-01 11:04:30 -0400

    Update client.js

--- client.js
+++ client.js
@@ -29875,6 +29875,61 @@ _0x2af9ee = aniChoice_startT + 1000 * timeoutS;
       }
       break;
 
+
+       case 61: //battleroyalmessages
+      {
+        var evType = msg.readUInt8();
+
+
+	      screenMessage = function (screenTextEndT, message) {
+    var fadeDur = 1.0;
+    var a = (screenTextEndT - timestamp) / 1000.0 / fadeDur;
+    a = 0 > a ? 0 : 1 < a ? 1 : a; //clamp from 0-1
+    if (a > 0) {
+      ctx.save();
+      var bx = canvasW / 2;
+      var barH = 60 * interfS;
+      var barW = (canvasW / 2 - 100) * interfS;
+      ctx.globalAlpha = 0.35;
+      ctx.fillStyle = "black"; //bar bg
+      var by = canvasH * 0.1;
+      ctx.fillRect(bx - barW / 2, by - barH / 2, barW, barH); //bg
+      ctx.globalAlpha = 1;
+      ctx.globalAlpha = a;
+  
+      ctx.globalAlpha = 1;
+      ctx.fillStyle = "white";
+      ctx.font = 40.0 * interfS + "px Arial";
+      ctx.lineWidth = 1;
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle"; //vertical center
+      ctx.shadowOffsetX = 1;
+      ctx.shadowOffsetY = 1;
+      ctx.shadowColor = "black";
+  
+      ctx.fillText(message, canvasW / 2, by);
+
+  
+      ctx.restore();
+    }
+};
+
+
+
+	      
+        switch (evType) {
+            
+          case 1://br timer
+            {
+         screenMessage(timestamp + 3500.0, "hi guys");
+            }
+            break;
+
+	}
+      }
+      break;
+		  
+		  
     case 23: //MsgPersonalGameEvent
       {
         var evType = msg.readUInt8();
@@ -30194,7 +30249,6 @@ var sound_click = getLazyLoadAudio("audio/" + sound + ".mp3");
       }
       break;
 
-   
      
     case 60: // Msg_goalScoredByOther
       {
