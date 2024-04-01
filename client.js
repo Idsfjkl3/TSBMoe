0d307e444a52c3a7e91a39297fa76d9a8270c306
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-04-01 18:19:22 -0400

    Update client.js

--- client.js
+++ client.js
@@ -29889,11 +29889,11 @@ _0x2af9ee = aniChoice_startT + 1000 * timeoutS;
             {
 		    
 
-  var ctx_ = miniMapCanvas.getContext("2d");
+  var ctx_ =  canvas.getContext("2d");
 
   ctx_.globalAlpha = 0.35;
   ctx_.fillStyle = "#000000";
-  ctx_.fillRect( 0 - canvas.width/2, 0 - canvas.height/2, miniMapCanvas.width, miniMapCanvas.height);
+  ctx_.fillRect(0, 0, miniMapCanvas.width, miniMapCanvas.height);
 		    		    console.log(ctx)
               screenText = "Ouch! Your tail got bitten!";
                screenTextCol = "white"; //default color for event
