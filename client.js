83f22958b02547068eaaad2b52ad3685191088bc
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-04-11 19:30:30 -0400

    Update client.js

--- client.js
+++ client.js
@@ -17774,10 +17774,10 @@ this.kamuifilterswap = 1
 
 
 			  if (impactblindness) {
+				  this.filterswap = 1
 var style = "html {-webkit-filter: grayscale(100%) contrast(999999999999999999999999999999999999999999999%); -moz-filter: grayscale(100%) contrast(999999999999999999999999999999999999999999999%); -o-filter: grayscale(100%) contrast(999999999999999999999999999999999999999999999%); -ms-filter: grayscale(100%) contrast(999999999999999999999999999999999999999999999%); }";
 var head = document.getElementsByTagName("head")[0];
   var styleTag = document.createElement("style");
-this.filterswap = 1
  
   styleTag.type = "text/css";
   if (styleTag.styleSheet) {
