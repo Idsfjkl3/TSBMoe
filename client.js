be7c44e80ec954673eaec83091a4814592763846
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-05-06 19:52:21 -0400

    Update client.js

--- client.js
+++ client.js
@@ -24385,18 +24385,15 @@ Dragon.prototype.drawSkinCustomization = function() {//mark323
         //var fac0to1 = Math.min(1.0, (timestamp - this.spawnTime) / 300.0);
 
         //console.log("getAnimFrame:" + frame);
-        var extraRotate = -(-0.2 + frame) * toRadians(-90.0); //spin animation
-
-        //clip to sliwly show the claw
-        var rad = this.rad * 0.8;
+                  var extraRotate = -(-0.2 + frame) * toRadians(45.0);
+                          ctx.rotate(toRadians(15) + extraRotate);
         var imX = 0;
         var imY = 1.4 * this.rad;
         var imW = 1.5 * 0.8 * this.rad;
         var imH = 2.3 *  0.8 * this.rad;
-        var imAnchorX = 1.05;
+        var imAnchorX = -0.05;
         var imAnchorY = 1.6;
-                            var extraRotate = -(-0.2 + frame) * toRadians(-45.0);
-                          ctx.rotate(toRadians(-15) + extraRotate);
+        ctx.drawImage(theImg, imX + imW * -imAnchorX, imY + imH * -imAnchorY, imW, imH);
 
         ctx.restore();
       }
