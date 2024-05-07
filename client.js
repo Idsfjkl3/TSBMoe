e58d52d55f42edf811581dfe92dfd944ba175d5c
Idsfjkl <116563579+Idsfjkl@users.noreply.github.com>
2024-05-06 20:04:20 -0400

    Update client.js

--- client.js
+++ client.js
@@ -24367,7 +24367,7 @@ Dragon.prototype.drawSkinCustomization = function() {//mark323
         var imY = 1.4 * rad;
         var imW = 1.5 * 0.8 * rad;
         var imH = 2.3 *  0.8 * rad;
-        var imAnchorX = -0.025;
+        var imAnchorX = -1.10;
         var imAnchorY = 1.6;
         ctx.drawImage(theImg, imX + imW * -imAnchorX, imY + imH * -imAnchorY, imW, imH);
 
