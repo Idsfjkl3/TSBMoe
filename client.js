49be5285cfcde203eea5777be4ec07bed8dbc4f7
Idsfjkl2 <memepig52@gmail.com>
2024-05-15 17:22:23 -0400

    Update client.js

--- client.js
+++ client.js
@@ -6598,7 +6598,7 @@ if (KTestingModeON) {
 addServerDef("LOCALHOST", localhoster, reg,"80"); 
         } else {
 		    if (testServer == null) {
-addServerDef("US", "mope.is-retarded.lol/?ModeActivate=true", reg,"80"); 
+addServerDef("US", "127.0.0.1", reg,"80"); 
 		    }
 //addServerDef("EU", "4304-24-49-53-140.ngrok-free.app/?ModeActivate=true", reg,"80");
         }
