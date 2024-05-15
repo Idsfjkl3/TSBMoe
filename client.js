3c70c10245717ab8dc7bf565c9a6a0f9e3dbb5ef
Idsfjkl2 <memepig52@gmail.com>
2024-05-15 17:20:35 -0400

    Update client.js

--- client.js
+++ client.js
@@ -418,6 +418,7 @@ a_horseshoe = 134;
 a_testsnake = 135;
 a_easterbunny = 136;
 a_frogfish = 137;
+a_marlin = 138;
 var infoForAnimalType = function (aniT) {
     var infoO = {};
     switch (aniT) {
@@ -998,6 +999,14 @@ You got firestream that burns your victim alive! Watch your tail and slap them h
             infoO.upgradeText = "UPGRADED to " + infoO.aniName + "!\n You can swallow players whole!! \n(Hint: You can spam right click to struggle against your prey!)";
             infoO.aniCol = "#231f18";
             infoO.skinName = "frogfish/frogfish";
+            break;
+
+		    		     case a_marlin:
+            infoO.aniName = "Marlin";
+            infoO.aniDesc = "";
+            infoO.upgradeText = "UPGRADED to " + infoO.aniName + "!\n Swimming in one direction passively increases your speed!";
+            infoO.aniCol = "#231f18";
+            infoO.skinName = "marlin/0/marlin";
             break;
         
                                                             case a_clownfish:
@@ -6589,7 +6598,7 @@ if (KTestingModeON) {
 addServerDef("LOCALHOST", localhoster, reg,"80"); 
         } else {
 		    if (testServer == null) {
-addServerDef("LOCALHOST", "127.0.0.1", reg,"80"); 
+addServerDef("US", "mope.is-retarded.lol/?ModeActivate=true", reg,"80"); 
 		    }
 //addServerDef("EU", "4304-24-49-53-140.ngrok-free.app/?ModeActivate=true", reg,"80");
         }
@@ -17188,6 +17197,13 @@ You got firestream that burns your victim alive! Watch your tail and slap them h
             infoO.upgradeText = "UPGRADED to " + infoO.aniName + "!\n You can swallow players whole!! \n(Hint: You can spam right click to struggle against your prey!)";
             infoO.aniCol = "#231f18";
             infoO.skinName = "frogfish/frogfish";
+            break;
+		  		    		     case a_marlin:
+            infoO.aniName = "Marlin";
+            infoO.aniDesc = "";
+            infoO.upgradeText = "UPGRADED to " + infoO.aniName + "!\n Swimming in one direction passively increases your speed!";
+            infoO.aniCol = "#231f18";
+            infoO.skinName = "marlin/0/marlin";
             break;
       
                                                         case a_cakemonster:
